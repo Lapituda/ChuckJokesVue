e23470d4eb5133a1ae2bb80bc1340701d60a1d49
Lapituda <Angeltod08@gmail.com>
2024-04-19 05:05:42 -0600

    confirmacion

---
+++ src/components/ChuckCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="col-lg-4 col-md-6 col-sm-12 mb-4">
+    <div class="card h-100">
+      <img :src="iconUrl" class="card-img-top" alt="Chuck Norris Icon">
+      <div class="card-body">
+        <p class="card-text">{{ value }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    iconUrl: String,
+    value: String
+  }
+};
+</script>
+
+<style scoped>
+/* Agrega estilos personalizados aquí si es necesario */
+</style>
